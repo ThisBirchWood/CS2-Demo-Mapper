0b58ceeda7e5717706498be874190eee163ae910
ThisBirchWood <dylanseandefaoite@gmail.com>
2025-03-16 12:21:46 +0100

    PATCH: Incorrect team numbers

--- main.py
+++ main.py
@@ -7,24 +7,23 @@ def main():
     import demoparser2
 
     demo_parser = demoparser2.DemoParser("demo.dem")
-    game_info = demo_parser.parse_ticks(["X", "Y", "Z", "pitch", "yaw", "is_alive", "team", "player_steamid", "team_rounds_total"])
+    game_info = demo_parser.parse_ticks(["X", "Y", "Z", "pitch", "yaw", "is_alive", "team", "player_steamid", "team_rounds_total", "team_num"])
     header_info = demo_parser.parse_header()
     map_name = header_info['map_name']
     players = demo_parser.parse_player_info()
 
     start_tick = int(input())
-    team_1 = Team()
+    team_1 = Team(is_ct=True)
     team_2 = Team()
     m = Match(map_name, game_info, team_1, team_2)
     m.tick = start_tick
     for index, row in players.iterrows():
-        if row["team_number"] == 1:
+        if row["team_number"] == 2:
             team_1.add_player(Player(row["name"], row["steamid"]))
-        else: 
+        elif row["team_number"] == 3: 
             team_2.add_player(Player(row["name"], row["steamid"]))
 
     game = Game(m)
-    
     game.run()
 
 if __name__ == "__main__":
