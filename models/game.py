14462ca4a830a8a7eb8473b5694b80d956933c25
ThisBirchWood <dylanseandefaoite@gmail.com>
2025-03-19 19:09:02 +0100

    REMOVE: game init

--- models/game.py
+++ models/game.py
@@ -22,7 +22,6 @@ class Game:
         self.match = match
         self.renderer = Renderer(self.match, self.screen)
         
-
     def handle_events(self):
         """Handles user inputs."""
         for event in pygame.event.get():
@@ -49,27 +48,4 @@ class Game:
             self.clock.tick(self.match.tick_rate)
 
         pygame.quit()
-
-if __name__ == "__main__":
-    import demoparser2
-
-    demo_parser = demoparser2.DemoParser("demo.dem")
-    game_info = demo_parser.parse_ticks(["X", "Y", "Z", "pitch", "yaw", "is_alive", "team", "player_steamid", "team_rounds_total"])
-    header_info = demo_parser.parse_header()
-    map_name = header_info['map_name']
-    players = demo_parser.parse_player_info()
-
-    start_tick = int(input())
-    team_1 = Team()
-    team_2 = Team()
-    m = Match(map_name, game_info, team_1, team_2)
-    m.tick = start_tick
-    for index, row in players.iterrows():
-        if row["team_number"] == 1:
-            team_1.add_player(Player(row["name"], row["steamid"]))
-        else: 
-            team_2.add_player(Player(row["name"], row["steamid"]))
-
-    game = Game(m)
     
-    game.run()
